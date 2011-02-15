c2302926e9075a545d4aa43384a68ce035876625
Rafal Piekarski <ravbaker@gmail.com>
2011-02-15 20:27:40 +0100

    podstawy pod projekt przykladowy

---
+++ example_notepad_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleNotepadApp::Application.config.secret_token = '581f375ded34423d46f1ef9958b9cfbdc7a7d1e5f623125f2d53d0f1817d0362eb98a2b9d3ab97bed1568e1e13dd0e5d0a072a40d464847284933dc60850e55e'
