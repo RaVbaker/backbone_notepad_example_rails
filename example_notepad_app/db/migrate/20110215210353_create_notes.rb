e3ef20d8cde31327f786ff1584d4eff5866a37a0
Rafal Piekarski <ravbaker@gmail.com>
2011-02-15 22:55:26 +0100

    postawienie podstaw pod notesy...

---
+++ example_notepad_app/db/migrate/20110215210353_create_notes.rb
@@ -0,0 +1,15 @@
+class CreateNotes < ActiveRecord::Migration
+  def self.up
+    create_table :notes do |t|
+      t.string :title
+      t.string :author
+      t.text :content
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :notes
+  end
+end
---
+++ example_notepad_app/test/unit/helpers/notepad_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class NotepadHelperTest < ActionView::TestCase
+end
