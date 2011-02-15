612f5f920257504f3878343940b47e2b21ad0413
Rafal Piekarski <ravbaker@gmail.com>
2011-02-15 20:34:23 +0100

    instalujemy jquery

--- commandline.sh
+++ commandline.sh
@@ -1,2 +1,14 @@
+# tworzymy aplikację: (bez Prototype jak już ustaliliśmy)
 rails new example_notepad_app -J 
-cd example_notepad_app
+cd example_notepad_app                                  
+
+# dodajemy obsluge jQuery (via: https://github.com/rails/jquery-ujs/blob/master/README.md ):
+echo "gem 'jquery-rails', '>= 0.2.6'" >> Gemfile
+
+#instalujemy potrzebne gemy:                          
+bundle install              
+                             
+# dodajemy jquery do aplikacji (z obsługa UI)
+rails generate jquery:install --ui
+
+
